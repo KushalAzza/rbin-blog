695601039b6d826ac1e45119597ef577ae551d08
Kushal Azza <KushalAzza@users.noreply.github.com>
2019-06-04 21:05:59 +0530

    Update and rename pwabuilder-sw-register.js to pwabuilder-sw-register-v1.js

--- pwa/pwabuilder-sw-register-v1.js
+++ pwa/pwabuilder-sw-register-v1.js
@@ -1,7 +1,3 @@
-// This is the "Offline page" service worker
-
-// Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
-
 // Check compatibility for the browser we're running this in
 if ("serviceWorker" in navigator) {
   if (navigator.serviceWorker.controller) {
@@ -9,7 +5,7 @@ if ("serviceWorker" in navigator) {
   } else {
     // Register the service worker
     navigator.serviceWorker
-      .register("pwabuilder-sw.js", {
+      .register("https://cdn.jsdelivr.net/gh/KushalAzza/rbin-blog/pwa/pwabuilder-sw.js", {
         scope: "./"
       })
       .then(function (reg) {
